94c7ce51dd88e2e3ec7a7564e2c07842ba96b139
Hristo Ivanov <47400757+arjunah@users.noreply.github.com>
2019-08-07 18:04:19 +0300

    wip team manager

---
+++ JS Apps/04. JS-Applications-Routing-and-Architecture-Lab-Skeleton/Team-Manager-Skeleton/scripts/app.js
@@ -0,0 +1,12 @@
+const app = Sammy("#main", function() {
+    this.use("Handlebars", "hbs")
+
+    this.get("#/home", router.loadHome);
+
+    this.get("#/about", router.loadAbout);
+    
+    this.get("#/register", router.loadRegister);
+    this.post("#/register", userController.register);
+});
+
+(() => app.run("#/home"))();
